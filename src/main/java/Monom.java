0d54ecaaf0bd4e5b7b6a76a66840fb2dec7e5932
Mark Ciurea <ciureamark@yahoo.com>
2022-02-17 22:01:24 +0200

    Monom, Polynom

---
+++ src/main/java/Monom.java
@@ -0,0 +1,9 @@
+public class Monom {
+    private double coef;
+    private int pow;
+
+    public Monom(double coef, int pow) {
+        this.coef = coef;
+        this.pow = pow;
+    }
+}
---
+++ src/main/java/Polynom.java
@@ -0,0 +1,5 @@
+import java.util.List;
+
+public class Polynom {
+    List<Monom> polinom;
+}
