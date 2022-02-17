316c2dac9dd6dc566990db52e91372edff8f17d4
Mark Ciurea <ciureamark@yahoo.com>
2022-02-17 15:09:55 +0200

    an other useless modification

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World 2");
-        int x;
+        System.out.println("Hello, developer!");
     }
 }
